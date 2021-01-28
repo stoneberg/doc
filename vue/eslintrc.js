a54b38c799a2d785ffe286c4a0dd62cb821f4160
Lee Yu Pyeong <stoneberg73@gmail.com>
2021-01-29 02:23:36 +0900

    eslint

--- vue/eslintrc.js
+++ vue/eslintrc.js
@@ -1,6 +1,57 @@
 module.exports = {
   root: true,
   env: {
+    node: true
+  },
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
+};
+
+
+--- remove kebab, airbnb
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/prettier'
+  ],
+  rules: {
+	'vue/no-multiple-template-root': 0,
+    'comma-dangle': 'off',
+    'class-methods-use-this': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'import/prefer-default-export': 'off',
+    "vue/component-name-in-template-casing": ["error", {
+      "ignores": []
+    }],
+    'prettier/prettier': ['error', { 'singleQuote': true, 'endOfLine': 'auto' }],
+	"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  },
+};
+
+
+---- original
+module.exports = {
+  root: true,
+  env: {
     browser: true,
   },
   parserOptions: {
@@ -9,7 +60,7 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
-    'plugin:vue/recommended',
+    'plugin:vue/vue3-essential',
     'prettier/vue',
     'plugin:prettier/recommended'
   ],
@@ -27,3 +78,37 @@ module.exports = {
     'prettier/prettier': ['error', { 'singleQuote': true, 'endOfLine': 'auto' }]
   },
 };
+
+eslintrc.js
+
+------------------- 2021.01.29 수정
+
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'prettier/vue',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+	'vue/no-multiple-template-root': 0,
+    'comma-dangle': 'off',
+    'class-methods-use-this': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': ['error', { 'singleQuote': true, 'endOfLine': 'auto' }]
+  },
+};
+
+
+
